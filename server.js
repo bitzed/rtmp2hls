342a79c83eb83586fabc9b5d824485b44568d6cc
bizted <offpiste8510@gmail.com>
2025-02-06 15:39:34 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+import NodeMediaServer from "node-media-server";
+import express from "express";
+
+const config = {
+    rtmp: {
+        port: 8080,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8080,
+        mediaroot: './media',
+        allow_origin: "*"
+    },
+    trans: {
+        ffmpeg: process.env.FFMPEG_PATH || '/usr/bin/ffmpeg',
+        tasks: [
+            {
+                app: 'live',
+                hls: true,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+            }
+        ]
+    }
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
+
+const app = express();
+app.use(express.static("public"));
+
+app.get('/stream', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
